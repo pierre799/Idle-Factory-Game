5ae0145fa9b15af67bde7eedcea453c0ba023cbe
πr <137527272+pierre799@users.noreply.github.com>
2025-03-31 22:17:46 +0200

    travaux sur la barre de progression

--- js/app.js
+++ js/app.js
@@ -1,12 +1,27 @@
-const btn = document.getElementById("boutonMagique");
-const expl = document.getElementById("texteMagique");
+const btnCommencer = document.getElementById("btnCommencer");
+const barreRemplissage = document.getElementById("barreRemplissage");
+const barreContenu = document.getElementById("barreContenu");
 
-btn.addEventListener('click', fonctionMagique);
+btnCommencer.addEventListener('click', () => compteRebours("10"));
 
-function fonctionMagique(){
-	expl.textContent = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+function compteRebours(secondes){
+	btnCommencer.disabled = true;
 
-	expl.style.fontSize = "27pt";
-	expl.style.color = "red";
-	expl.style.backgroundColor = "yellow";
+    let tempsRestant = secondes;
+    
+    let intervalle = setInterval(() => {
+		tempsRestant--;
+        let pourcentage = tempsRestant*(100/secondes);
+		barreRemplissage.style.width = pourcentage + "%";
+		
+        if (pourcentage <= 10) {
+            barreContenu.style.backgroundColor = "rgb(121, 38, 38)";
+        }
+
+        if (tempsRestant < 0) {
+            clearInterval(intervalle);
+			barreRemplissage.style.width = "0%";
+			btnCommencer.disabled = false;
+        }
+    }, 1000);
 }
