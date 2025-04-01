27af8381382f90b49c9a4615d6633a27e5483c4d
πr <137527272+pierre799@users.noreply.github.com>
2025-04-01 22:18:38 +0200

    modifications

--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
-const fenetreJeu = document.getElementById("fenetreJeu");
 const btnCommencer = document.getElementById("btnCommencer");
 const barreRemplissage = document.getElementById("barreRemplissage");
-const barreContenu = document.getElementById("barreContenu");
 const curseurCentre = document.getElementById("curseurCentre");
+const barreConbarreContourtenu = document.getElementById("barreContour");
+const fenetreJeuMain = document.getElementById("fenetreJeuMain")
 
-const evenement = document.event;
-
-btnCommencer.addEventListener('click', () => compteRebours("30"));
+btnCommencer.addEventListener('click', () => compteRebours("3"));
 
 function compteRebours(secondes){
 	btnCommencer.disabled = true;
@@ -15,27 +13,27 @@ function compteRebours(secondes){
     
     let intervalle = setInterval(() => {
 		tempsRestant--;
-        let pourcentage = tempsRestant*(100/secondes);
+        let pourcentage = tempsRestant * (100 / secondes);
 		barreRemplissage.style.width = pourcentage + "%";
 		
-        if (pourcentage <= 10) {
-            barreContenu.style.backgroundColor = "rgb(121, 38, 38)";
+        if (pourcentage <= 15) {
+            barreContour.style.boxShadow = "inset 0 0 0px 3px rgb(255, 105, 46), inset 0 0 0px 4px #000000,  inset 0 0 4px 4px #000000";
         }
-
+        console.log(pourcentage);
         if (tempsRestant < 0) {
             clearInterval(intervalle);
-            barreRemplissage.style.width = "100%"
-            barreContenu.style.backgroundColor = "rgb(60, 55, 50)";
+            barreContour.style.boxShadow = "inset 0 0 0px 3px #FFE62B, inset 0 0 0px 4px #000000,  inset 0 0 4px 4px #000000";
+			barreRemplissage.style.width = "100%";
 			btnCommencer.disabled = false;
         }
     }, 1000);
 }
 
-/// tests
-
 function posCurseur(event) {
-    let x = event.pageX; //fenetreJeu.offsetLeft
-    let y = event.pageY; //fenetreJeu.offsetTop
-    curseurCentre.style.left=`${x-25}px`; //changer à - la moitié de la largeur
-    curseurCentre.style.top=`${y-25}px`;
+    let rect = fenetreJeuMain.getBoundingClientRect(); // Get container position
+    let x = event.clientX - rect.left; // Get x relative to the container
+    let y = event.clientY - rect.top;  // Get y relative to the container
+
+    curseurCentre.style.left = `${x - 800}px`;
+    curseurCentre.style.top = `${y - 800}px`;
 }
