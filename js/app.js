7366d3560912933cf95c2e8f8d6e47e9f704e8af
πr <137527272+pierre799@users.noreply.github.com>
2025-04-02 21:03:41 +0200

    modifications

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
-const btnCommencer = document.getElementById("btnCommencer");
 const barreRemplissage = document.getElementById("barreRemplissage");
 const curseurCentre = document.getElementById("curseurCentre");
 const barreConbarreContourtenu = document.getElementById("barreContour");
-const fenetreJeuMain = document.getElementById("fenetreJeuMain")
+const fenetreJeuMain = document.getElementById("fenetreJeuMain");
+const texteRound = document.getElementById("texteRound");
+const tableauResultats = document.getElementById("tableauResultats");
 
-btnCommencer.addEventListener('click', () => compteRebours("7"));
+var enJeu = false;
+var resultats = ["NAIcon.png","looseIcon.png","winIcon.png"];
+var round = 0;
+
+//const firstTh = tableauResultats.getElementsByTagName("th")[round];
+//const img = firstTh.querySelector("img");
+//img.src = "ressources/images/icones/winIcon.png";
 
 function compteRebours(secondes){
-	btnCommencer.disabled = true;
-
+	enJeu = true;
+    texteRound.textContent = `Round ${round+1}/5`;
     let tempsRestant = secondes;
     
     let intervalle = setInterval(() => {
@@ -17,14 +24,16 @@ function compteRebours(secondes){
 		barreRemplissage.style.width = pourcentage + "%";
 		
         if (pourcentage <= 15) {
-            barreContour.style.boxShadow = "inset 0 0 0px 3px rgb(255, 105, 46), inset 0 0 0px 4px #000000,  inset 0 0 4px 4px #000000";
+            barreContour.style.boxShadow = "box-shadow: inset 0 0 0 0.5vh rgb(255, 105, 46), inset 0 0 0 0.65vh #000000, inset 0 0 0.5vh 0.65vh #000000";
         }
-        console.log(pourcentage);
         if (tempsRestant < 0) {
             clearInterval(intervalle);
-            barreContour.style.boxShadow = "inset 0 0 0px 3px #FFE62B, inset 0 0 0px 4px #000000,  inset 0 0 4px 4px #000000";
+            barreContour.style.boxShadow = "box-shadow: inset 0 0 0 0.5vh #FFE62B, inset 0 0 0 0.65vh #000000, inset 0 0 0.5vh 0.65vh #000000";
 			barreRemplissage.style.width = "100%";
-			btnCommencer.disabled = false;
+
+            
+            round ++;
+			enJeu = false;
         }
     }, 1000);
 }
@@ -34,6 +43,13 @@ function posCurseur(event) {
     let x = event.clientX - rect.left; // Get x relative to the container
     let y = event.clientY - rect.top;  // Get y relative to the container
 
-    curseurCentre.style.left = `${x - 800}px`;
-    curseurCentre.style.top = `${y - 800}px`;
+    curseurCentre.style.left = `${x - curseurCentre.offsetWidth / 2}px`;
+    curseurCentre.style.top = `${y - curseurCentre.offsetHeight / 2}px`;
+}
+
+function interactionCurseur(event){
+    //verifier les etats pour savoir si en jeu ou non, si pas en jeu, on en lance un
+    if (enJeu == false){
+        compteRebours("10");
+    }
 }
