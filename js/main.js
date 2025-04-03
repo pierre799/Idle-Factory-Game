941e48376743085f3c5dda3f7a008dab81c7ef96
πr <137527272+pierre799@users.noreply.github.com>
2025-04-03 22:00:47 +0200

    modifications

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-const barFill = document.getElementById("barFill");
-const barOutline = document.getElementById("barOutline");
+const gameArea = document.getElementById("gameArea")
+const progressBarFill = document.getElementById("progressBarFill");
+const progressBarOutline = document.getElementById("progressBarOutline");
 const balanceText = document.getElementById("balanceText");
 
 var isInGame = false;
@@ -8,28 +9,28 @@ var balance = 0;
 //renommer et "inverser" la fonction 
 function countdown(seconds) {
     isInGame = true;
-    balanceText.textContent = `${balance + 1}`;
+    balance++;
     let remainingTime = seconds;
     
     let interval = setInterval(() => {
         remainingTime--;
         let percentage = remainingTime * (100 / seconds);
-        barFill.style.width = percentage + "%";
+        progressBarFill.style.width = percentage + "%";
         
         if (percentage <= 15) {
-            barOutline.style.boxShadow = "box-shadow: inset 0 0 0 0.5vh rgb(255, 105, 46), inset 0 0 0 0.65vh #000000, inset 0 0 0.5vh 0.65vh #000000";
+            progressBarOutline.style.boxShadow = "box-shadow: inset 0 0 0 0.5vh rgb(255, 105, 46), inset 0 0 0 0.65vh #000000, inset 0 0 0.5vh 0.65vh #000000";
         }
         if (remainingTime < 0) {
             clearInterval(interval);
-            barOutline.style.boxShadow = "box-shadow: inset 0 0 0 0.5vh #FFE62B, inset 0 0 0 0.65vh #000000, inset 0 0 0.5vh 0.65vh #000000";
-            barFill.style.width = "100%";
-            balance++;
+            progressBarOutline.style.boxShadow = "box-shadow: inset 0 0 0 0.5vh #FFE62B, inset 0 0 0 0.65vh #000000, inset 0 0 0.5vh 0.65vh #000000";
+            progressBarFill.style.width = "100%";
+            balanceText.textContent = `${balance}`;
             isInGame = false;
         }
     }, 1000);
 }
 
-function cursorInteraction(event) {
+function frameClicked(event) {
     if (isInGame == false) {
         countdown(5);
     }
